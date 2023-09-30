87a58fc6d5d357b7dd3125e0de2fde25c5f63059
kgand <34491606+kgand@users.noreply.github.com>
2023-09-30 14:57:34 -0400

    Create alerts.py

---
+++ alerts.py
@@ -0,0 +1,8 @@
+# alerts.py
+
+# Function to generate alerts (simplified example)
+def generate_alert(packet):
+    # Generate an alert based on the detected threat
+    # Example: Print an alert message
+    print("ALERT: Suspicious network traffic detected!")
+    print(packet.summary())
